025202fcd48b2b30a1315c346f513ad6954f668e
David Kuo <s50407s@gmail.com>
2018-06-10 20:03:54 +0800

    Update crawl.sh

--- crawl.sh
+++ crawl.sh
@@ -10,7 +10,7 @@ for lang in "${LANGS[@]}"; do
 	mkdir -p "dist/$lang"
 
 	# Fetch staffs
-	curl -Lo "dist/$lang/staffs.json" "https://script.google.com/a/coscup.tw/macros/s/AKfycbxz2kOgLEPrk1fjvO6jWAFdOKkQquR4SpU2R2Zv6KgEvwXqk5Di/exec"
+	curl -Lo "dist/$lang/staffs.json" "https://script.google.com/a/coscup.tw/macros/s/AKfycbxz2kOgLEPrk1fjvO6jWAFdOKkQquR4SpU2R2Zv6KgEvwXqk5Di/exec?lang=$lang"
 
 	# Fetch sponsors
 	curl -Lo "dist/$lang/sponsors.json" "https://script.google.com/macros/s/AKfycbx49lzZ60QVcHXFmrqLFwOi4j09r3WnBQQCaFao_BGNOT8gukM/exec?lang=$lang"

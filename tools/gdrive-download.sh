a9aea563b54d2d62020d336ab9b96ebade1356b6
David Kuo <s50407s@gmail.com>
2018-06-14 19:43:36 +0800

    [CI] Process sponsor's logo image

--- tools/gdrive-download.sh
+++ tools/gdrive-download.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-function gdrive_download() {
-	local FILE_ID="$1"; shift
-	local FILENAME="$1"; shift
+function gdrive_download {
+	local FILE_ID="$1"
+	local FILENAME="$2"
 
 	curl -L -o "${FILENAME}" "https://drive.google.com/uc?export=view&id=${FILE_ID}"
 }
 
-gdrive_download $1 $2
+gdrive_download "$1" "$2"
